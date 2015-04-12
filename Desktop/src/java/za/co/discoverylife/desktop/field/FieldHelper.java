0013969c3de81324d9e15fa6ec06128b5501a649
Anton Schoultz <AntonSchoultz@gmail.com>
2015-04-12 15:21:48 +0200

    Added field copying

--- Desktop/src/java/za/co/discoverylife/desktop/field/FieldHelper.java
+++ Desktop/src/java/za/co/discoverylife/desktop/field/FieldHelper.java
@@ -1,8 +1,15 @@
 package za.co.discoverylife.desktop.field;
 
+import java.io.File;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+//import java.util.Collection;
 
-public class FieldHelper implements IFieldTypes{
+import java.util.Date;
+
+import za.co.discoverylife.desktop.util.DateTime;
+
+public class FieldHelper implements IFieldTypes {
 	public Field field;
 	public int fieldTypeCd;
 
@@ -11,34 +18,193 @@ public class FieldHelper implements IFieldTypes{
 		this.field.setAccessible(true);
 		fieldTypeCd = getFieldType(field.getClass());
 	}
-	
-	  /**
-	   * Get field type code for the given class
-	   * 
-	   * @param fldClass
-	   * @return
-	   */
-	  public static int getFieldType(Class<?> fldClass)
-	  {
-	    int rtn = 0;
-	    for (int i = 1; i < CLASS.length; i++)
-	    {
-	      if ( fldClass.equals(CLASS[i]) )
-	      {
-	        rtn = i;
-	        break;
-	      }
-	    }
-	    for (int i = 1; i < CLASSJ.length; i++)
-	    {
-	      if ( CLASSJ[i].isAssignableFrom(fldClass) )
-	      {
-	        rtn = i;
-	        break;
-	      }
-	    }
-	    return rtn;
-	  }
+
+	/** Returns true if the field is a 'simple'/'native' type */
+	public boolean isSimple() {
+		return fieldTypeCd > 0 && fieldTypeCd < TYPE_COLLECTION;
+	}
+
+	/** get field value as a String */
+	public String getStringFromField(Object object) {
+		try {
+			if (fieldTypeCd == TYPE_DATE) {
+				DateTime dtm = new DateTime((Date) field.get(object));
+				return dtm.toString();
+			}
+			return String.valueOf(field.get(object));
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * Sets the specified Field of the object from the string value provided
+	 */
+	public void setFieldFromString(Object targetObject, String value) {
+		try {
+			if (value == null) {
+				return;
+			}
+			if ("null".equals(value)) {
+				field.set(targetObject, null);
+				return;
+			}
+			Object v = getValueAsObject(value);
+			if (v != null) {
+				field.set(targetObject, v);
+			} else {
+				throw new Exception("Could not set " + field.getName()
+						+ " from " + value);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * convert the simple string representation into the appropriate object
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public Object getValueAsObject(String value) {
+		Object v = null;
+		Class<? extends Object> fldClass = field.getType();
+		int ty = getFieldType(fldClass);
+
+		switch (ty) {
+		// case TYPE_COLLECTION :
+		// String[] sa = value.substring(1, value.length() - 1).split(",");
+		// try
+		// {
+		// Class innerType = Class.forName(getFieldInnerType());
+		// if ( innerType == java.lang.String.class )
+		// {
+		// Collection<String> col = (Collection<String>) getFieldObject();
+		// v = col;
+		// for (String s : sa)
+		// {
+		// s = s.trim();
+		// if ( !col.contains(s) )
+		// {
+		// col.add(s);
+		// }
+		// }
+		// }
+		// }
+		// catch (Exception e1)
+		// {
+		// e1.printStackTrace();
+		// }
+		// break;
+		case TYPE_DATE:
+			DateTime dt = new DateTime(value);
+			v = dt.getAsDate();
+			break;
+		case TYPE_OBJECT:
+		case TYPE_STRING:
+			v = value;
+			break;
+		case TYPE_CHAR:
+			v = (char) Integer.parseInt(value);
+			break;
+		case TYPE_SHORT:
+			v = Short.parseShort(value);
+			break;
+		case TYPE_INT:
+			v = Integer.parseInt(value);
+			break;
+		case TYPE_LONG:
+			v = Long.parseLong(value);
+			break;
+		case TYPE_DOUBLE:
+			v = Double.parseDouble(value);
+			break;
+		case TYPE_FLOAT:
+			v = Float.parseFloat(value);
+			break;
+		case TYPE_BOOLEAN:
+			v = Boolean.parseBoolean(value);
+			break;
+		case TYPE_FILE:
+			v = new File(value);
+			break;
+		}
+		if (v == null) {
+			// type not known - try constructor which takes a string value
+			try {
+				Constructor<? extends Object> constr = fldClass
+						.getConstructor(String.class);
+				v = constr.newInstance(value);
+			} catch (Exception e) {
+				// e.printStackTrace();
+			}
+		}
+		return v;
+	}
+
+	/**
+	 * Return the field as an object
+	 * 
+	 * @return field object
+	 */
+	public Object getFieldAsObject(Object object) {
+		Object o = null;
+		try {
+			o = field.get(object);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return o;
+	}
+
+	/**
+	 * Return the generic type for a collection/compound field (type of objects)
+	 */
+	public String getFieldInnerType() {
+		String type = field.getGenericType().toString();
+		int ix = type.indexOf("<");
+		if (ix >= 0) {
+			type = type.substring(ix + 1, type.length() - 1);
+		}
+		return type;
+	}
+
+	/** Return collection type */
+	public String getFieldOuterType() {
+		String type = field.getGenericType().toString();
+		int ix = type.indexOf("<");
+		if (ix >= 0) {
+			type = type.substring(0, ix);
+		}
+		return type;
+	}
+
+	/**
+	 * Get field type code for the given class
+	 * 
+	 * @param fldClass
+	 * @return
+	 */
+	public static int getFieldType(Class<?> fldClass) {
+		int rtn = 0;
+		for (int i = 1; i < CLASS.length; i++) {
+			if (fldClass.equals(CLASS[i])) {
+				rtn = i;
+				break;
+			}
+		}
+		for (int i = 1; i < CLASSJ.length; i++) {
+			if (CLASSJ[i].isAssignableFrom(fldClass)) {
+				rtn = i;
+				break;
+			}
+		}
+		return rtn;
+	}
 
 	public Field getField() {
 		return field;
--- Desktop/src/java/za/co/discoverylife/desktop/field/ObjectHelper.java
+++ Desktop/src/java/za/co/discoverylife/desktop/field/ObjectHelper.java
@@ -4,14 +4,70 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
 
+import za.co.discoverylife.desktop.util.MRUHashMap;
+
+/**
+ * Provides access to data object via field names.
+ * 
+ * @author Anton
+ * 
+ */
 public class ObjectHelper {
-	private HashMap<String, FieldHelper> fldMap;
+	/** MostRecentlyUsed Cache map of class vs ObjectHelper */
+	private static MRUHashMap<Class<?>, ObjectHelper> map
+		= new MRUHashMap<Class<?>, ObjectHelper>(100);
 	
-	public ObjectHelper(Class<?> klass){
-		this(klass,true);
+	/** Returns an ObjectHelper for the provided class */
+	public static ObjectHelper getHelper(Class<?> klass){
+		ObjectHelper oh = map.get(klass);
+		if(oh==null){
+			oh = new ObjectHelper(klass);
+			map.put(klass, oh);
+		}
+		return oh;
+	}
+	
+	/** Sets the maximum size for the ObjectHelper's MRU cache */
+	public static void setMapSize(int size){
+		map.setMaxEntries(size);
+	}
+	
+	/** Copy a clone of source fields into matching fields in the target object */
+	public static void copy(Object source,Object target) throws Exception{
+		ObjectHelper ohSrc = getHelper(source.getClass());
+		ObjectHelper ohTgt = getHelper(target.getClass());
+		for(String fieldname:ohSrc.fldMap.keySet()){
+			Object value = ohSrc.cloneField(source, fieldname);// get cloned value
+			try {
+				ohTgt.setFieldObject(target, fieldname, value);// set into target
+			} catch (Exception e) {
+				// target field not found or incompatible;
+			}
+		}
 	}
 
-	public ObjectHelper(Class<?> klass, boolean doParentClass) {
+	private HashMap<String, FieldHelper> fldMap;
+
+	/**
+	 * CONSTRUCT an Object Helper for the provided class, navigating up the
+	 * extends tree to parent classes.
+	 * 
+	 * @param klass
+	 *            Class to construct Helper for.
+	 */
+	private ObjectHelper(Class<?> klass) {
+		this(klass, true);
+	}
+
+	/**
+	 * CONSTRUCT an Object Helper for the provided class.
+	 * 
+	 * @param klass
+	 *            Class to construct helper for
+	 * @param doParentClass
+	 *            true=navigate up the parent classes
+	 */
+	private ObjectHelper(Class<?> klass, boolean doParentClass) {
 		fldMap = new HashMap<String, FieldHelper>();
 		Class<?> k = klass;
 		while (k != null) {
@@ -35,15 +91,78 @@ public class ObjectHelper {
 		}
 	}
 	
-	public Object getFieldObject(Object object,String fieldname) throws Exception{
-		FieldHelper fh = fldMap.get(fieldname);
-		if(fh==null) throw new Exception("Invalid field name '"+fieldname+"'");
-		return fh.getField().get(object);
-	}
 	
-	public void setFieldObject(Object object,String fieldname,Object value) throws Exception{
-		FieldHelper fh = fldMap.get(fieldname);
-		if(fh==null) throw new Exception("Invalid field name '"+fieldname+"'");
-		fh.getField().set(object,value);
+
+	/**
+	 * Returns a clone copy of the sourceObject field value. <br>
+	 * (new instance/object, not a duplicate pointer)
+	 * 
+	 * @param sourceObject
+	 *            Object to copy the field from
+	 * @param fieldname
+	 *            name of the field to copy
+	 * @return Replica field value object
+	 * @throws Exception
+	 */
+	public Object cloneField(Object sourceObject, String fieldname)
+			throws Exception {
+		FieldHelper fh = findField(fieldname);
+		if (fh.isSimple()) {
+			return fh.getValueAsObject(fh.getStringFromField(sourceObject));
+		}
+		return null;
 	}
+
+	/**
+	 * Returns the value of the named field, on the specified object. 
+	 * The value is automatically wrapped in an object if it has a primitive type.
+	 * 
+	 * @param sourceObject
+	 *            Object to read the field from
+	 * @param fieldname
+	 *            name of the field to read
+	 * @return String representation of the filed value
+	 * @throws Exception
+	 *             Any error
+	 */
+	public Object getFieldObject(Object sourceObject, String fieldname)
+			throws Exception {
+		FieldHelper fh = findField(fieldname);
+		return fh.getField().get(sourceObject);
+	}
+
+	/**
+	 * Sets the named field in the target object from the string representation
+	 * provided
+	 * 
+	 * @param targetObject
+	 *            object to have it's field set
+	 * @param fieldname
+	 *            name of the field to set
+	 * @param value
+	 *            String representation of the new field value
+	 * @throws Exception
+	 *             Any errors
+	 */
+	public void setFieldObject(Object targetObject, String fieldname,
+			Object value) throws Exception {
+		FieldHelper fh = findField(fieldname);
+		fh.getField().set(targetObject, value);
+	}
+
+	/**
+	 * Returns the FieldHelper for the named field, Throws an exception if the
+	 * field name is not found
+	 * 
+	 * @param fieldname
+	 *            name of the field to access
+	 * @return FieldHelper for the field
+	 * @throws Exception
+	 *             If the field name is not found
+	 */
+	private FieldHelper findField(String fieldname) throws Exception {
+		FieldHelper fh = findField(fieldname);
+		return fh;
+	}
+
 }
